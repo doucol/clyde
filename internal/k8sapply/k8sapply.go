219bcd9520710058efcbc28b3ed6d11afcd8f80e
Doug Collier <540588+doucol@users.noreply.github.com>
2025-07-27 16:50:26 -0500

    fix up install of calico - wip

--- internal/k8sapply/k8sapply.go
+++ internal/k8sapply/k8sapply.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -29,7 +30,7 @@ type Applier struct {
 // NewApplier creates a new Applier instance
 func NewApplier(clientset kubernetes.Interface, dynamicClient dynamic.Interface, logger io.Writer) (*Applier, error) {
 	// Get all API resources
-	_, apiResourceLists, err := clientset.Discovery().ServerGroupsAndResources()
+	apiResourceLists, err := getAPIResources(clientset)
 	if err != nil {
 		return nil, err
 	}
@@ -40,6 +41,26 @@ func NewApplier(clientset kubernetes.Interface, dynamicClient dynamic.Interface,
 		logger:    logger,
 		resources: apiResourceLists,
 	}, nil
+}
+
+func getAPIResources(clientset kubernetes.Interface) ([]*metav1.APIResourceList, error) {
+	discoveryClient := clientset.Discovery()
+	apiGroups, err := discoveryClient.ServerGroups()
+	if err != nil {
+		return nil, err
+	}
+	retval := []*metav1.APIResourceList{}
+	for _, group := range apiGroups.Groups {
+		for _, version := range group.Versions {
+			resources, err := discoveryClient.ServerResourcesForGroupVersion(version.GroupVersion)
+			if err != nil {
+				// fmt.Printf("Skipping %s: %v\n", version.GroupVersion, err)
+				continue
+			}
+			retval = append(retval, resources)
+		}
+	}
+	return retval, nil
 }
 
 func (a *Applier) Logf(format string, args ...any) {
@@ -136,28 +157,46 @@ func (a *Applier) ApplyReader(ctx context.Context, reader io.Reader) error {
 
 // applyResource applies a single Kubernetes resource
 func (a *Applier) applyResource(ctx context.Context, obj *unstructured.Unstructured) error {
-	gvk := obj.GetObjectKind().GroupVersionKind()
+	gvk := obj.GroupVersionKind()
 	namespace := obj.GetNamespace()
-	if namespace == "" {
+
+	// Find the API resource for this GVK
+	apiResource, err := a.findAPIResourceForGVK(gvk)
+	if err != nil {
+		return fmt.Errorf("failed to find API resource for GVK %s/%s %s: %w",
+			gvk.GroupVersion().String(), gvk.Kind, obj.GetName(), err)
+	}
+
+	// Determine if the resource is namespaced
+	isNamespaced := apiResource.Namespaced
+	if isNamespaced && namespace == "" {
 		namespace = "default"
 	}
 
-	var err error
-	var gvr schema.GroupVersionResource
-	if gvr, err = a.findGVRForGVK(gvk); err != nil {
-		return err
-	}
-
-	// Get the dynamic client for this resource
+	gvr := gvk.GroupVersion().WithResource(apiResource.Name)
 	resourceClient := a.dynamic.Resource(gvr)
 
-	// Check if resource exists
-	existing, err := resourceClient.Namespace(namespace).Get(ctx, obj.GetName(), metav1.GetOptions{})
+	var existing *unstructured.Unstructured
+	if isNamespaced {
+		// For namespaced resources, use namespace
+		existing, err = resourceClient.Namespace(namespace).Get(ctx, obj.GetName(), metav1.GetOptions{})
+	} else {
+		// For non-namespaced resources, use cluster-scoped client
+		existing, err = resourceClient.Get(ctx, obj.GetName(), metav1.GetOptions{})
+	}
+
 	if err != nil {
 		// Resource doesn't exist, create it
-		_, err = resourceClient.Namespace(namespace).Create(ctx, obj, metav1.CreateOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to create %s %s: %w", gvk.Kind, obj.GetName(), err)
+		if isNamespaced {
+			_, err = resourceClient.Namespace(namespace).Create(ctx, obj, metav1.CreateOptions{})
+			if err != nil {
+				return fmt.Errorf("failed to create namespaced object %s %s: %w", gvk.Kind, obj.GetName(), err)
+			}
+		} else {
+			_, err = resourceClient.Create(ctx, obj, metav1.CreateOptions{})
+			if err != nil {
+				return fmt.Errorf("failed to create non-namespaced object %s %s: %w", gvk.Kind, obj.GetName(), err)
+			}
 		}
 		a.Logf("Created %s/%s %s", gvk.GroupVersion().String(), gvk.Kind, obj.GetName())
 		return nil
@@ -165,7 +204,11 @@ func (a *Applier) applyResource(ctx context.Context, obj *unstructured.Unstructu
 
 	// Resource exists, update it
 	obj.SetResourceVersion(existing.GetResourceVersion())
-	_, err = resourceClient.Namespace(namespace).Update(ctx, obj, metav1.UpdateOptions{})
+	if isNamespaced {
+		_, err = resourceClient.Namespace(namespace).Update(ctx, obj, metav1.UpdateOptions{})
+	} else {
+		_, err = resourceClient.Update(ctx, obj, metav1.UpdateOptions{})
+	}
 	if err != nil {
 		return fmt.Errorf("failed to update %s %s: %w", gvk.Kind, obj.GetName(), err)
 	}
@@ -174,23 +217,49 @@ func (a *Applier) applyResource(ctx context.Context, obj *unstructured.Unstructu
 	return nil
 }
 
-func (a *Applier) findGVRForGVK(gvk schema.GroupVersionKind) (schema.GroupVersionResource, error) {
+func (a *Applier) findAPIResourceForGVK(gvk schema.GroupVersionKind) (*metav1.APIResource, error) {
+	// First pass: exact match
 	for _, apiResourceList := range a.resources {
 		for _, apiResource := range apiResourceList.APIResources {
-			// log.Printf("API resource: %+v", apiResource)
 			version := apiResource.Version
 			if version == "" {
 				version = "v1"
 			}
 			if apiResource.Group == gvk.Group && version == gvk.Version && apiResource.Kind == gvk.Kind {
-				return schema.GroupVersionResource{
-					Group:    apiResource.Group,
-					Version:  version,
-					Resource: apiResource.Name,
-				}, nil
+				return &apiResource, nil
 			}
 		}
 	}
 
-	return schema.GroupVersionResource{}, fmt.Errorf("gvk not found: %s | %s | %s", gvk.Group, gvk.Version, gvk.Kind)
+	// Second pass: try to find by kind and group, ignoring version differences
+	// This handles cases where the API server might return different versions than expected
+	for _, apiResourceList := range a.resources {
+		for _, apiResource := range apiResourceList.APIResources {
+			if apiResource.Group == gvk.Group && apiResource.Kind == gvk.Kind {
+				return &apiResource, nil
+			}
+		}
+	}
+
+	// Third pass: try to find by kind, ignoring group and version differences
+	// This handles cases where the API server might return different versions than expected
+	for _, apiResourceList := range a.resources {
+		for _, apiResource := range apiResourceList.APIResources {
+			log.Printf("apiResource: %+v", apiResource)
+			if apiResource.Group == "" && apiResource.Version == "" && apiResource.Kind == gvk.Kind {
+				return &apiResource, nil
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("api resource not found: GVK: %s | %s | %s", gvk.Group, gvk.Version, gvk.Kind)
+}
+
+// buildGVRFromAPIResource constructs a GroupVersionResource from GVK and APIResource
+func (a *Applier) buildGVRFromAPIResource(apiResource *metav1.APIResource) schema.GroupVersionResource {
+	return schema.GroupVersionResource{
+		Group:    apiResource.Group,
+		Version:  apiResource.Version,
+		Resource: apiResource.Name,
+	}
 }
